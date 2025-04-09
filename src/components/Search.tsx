8bb4b7300630dcd3374354c9604ad282d4363aec
gato1706 <thiago.gato17g@gmail.com>
2025-04-09 14:35:25 -0300

    estilizando a interface

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,24 @@
-import React from "react";
 import { BsSearch } from "react-icons/bs";
 
-export const Search = () => {
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>;
+};
+
+import { useState } from "react";
+
+import classes from './Search.module.css'
+
+export const Search = ({ loadUser }: SearchProps) => {
+  const [userName, setUserName] = useState("");
+
   return (
-    <div>
+    <div className={classes.search}>
       <h2>Busque por um usuário:</h2>
-      <p>Cpmjeça seus melhores repositórios</p>
-      <div>
-        <input type="text" placeholder="Digite o nome do usuário" />
-        <button>
+      <p>Conheça seus melhores repositórios</p>
+
+      <div className={classes.search_container}>
+        <input type="text" placeholder="Digite o nome do usuário" onChange={(e) => setUserName(e.target.value)} />
+        <button onClick={()=> loadUser(userName)}>
           <BsSearch />
         </button>
       </div>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -6,9 +6,27 @@ import { UserProps } from "../types/user";
 export const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
 
+  const loadUser = async (userName: string) => {
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+
+    const data = await res.json();
+
+    const { avatar_url, login, location, followers, following } = data;
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      following,
+      followers,
+    };
+
+    setUser(userData);
+  };
+
   return (
     <div>
-      <Search />
+      <Search loadUser={loadUser} />
+      {user && <p>{user.login}</p>}
     </div>
   );
 };
