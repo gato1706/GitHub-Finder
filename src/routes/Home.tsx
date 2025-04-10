969aa189a1619f16c1cfb373ca95698b677d496f
gato1706 <thiago.gato17g@gmail.com>
2025-04-10 17:29:08 -0300

    criando a logica de erro de usuário não encontrado

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,14 +2,25 @@ import { Search } from "../components/Search";
 import { useState } from "react";
 import { UserProps } from "../types/user";
 import  User  from "../components/User";
+import Error from "../components/Error";
 
 export const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
 
+  const [error, setError] = useState(false)
+
   const loadUser = async (userName: string) => {
+    setError(false)
+    setUser(null)
+
     const res = await fetch(`https://api.github.com/users/${userName}`);
 
     const data = await res.json();
+
+    if(res.status ===404){
+      setError(true);
+      return;
+    }
 
     const { avatar_url, login, location, followers, following } = data;
     const userData: UserProps = {
@@ -27,6 +38,7 @@ export const Home = () => {
     <div>
       <Search loadUser={loadUser} />
       {user && <User {...user}/>}
+      {error && <Error />}
     </div>
   );
 };
